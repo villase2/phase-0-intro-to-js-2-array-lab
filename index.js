63e225262b67188ee58c3fb0df4d7b7fc07b4866
Steve Villanueva <villa_se@yahoo.com>
2023-01-30 15:06:07 -0600

    commit tree cabe960ceeb2a28ae1c8e64e05cd2005c629bca2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+   return appendCat = [...cats, "Broom"];
+}
+function prependCat(name) {
+    return prependCat = ["Arnold", ...cats];
+}
+function removeLastCat() {
+    return removeLastCat = cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return removeFirstCat = cats.slice(1, 3)
+}
